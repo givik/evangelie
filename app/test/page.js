cad10349bb2182cefdc79d15d97c7a6a346cf400
Givi Kuchukhidze <givik@users.noreply.github.com>
2026-01-28 00:59:13 +0400

    changes

--- app/test/page.js
+++ app/test/page.js
@@ -1,31 +1,3 @@
-import Dropdown from '@/components/Dropdown';
-
 export default function Test() {
-  return (
-    <div>
-      <Dropdown>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-        <div>item 1</div>
-        <div>item 2</div>
-        <div>item 3</div>
-      </Dropdown>
-    </div>
-  );
+  return <div>test ok</div>;
 }
