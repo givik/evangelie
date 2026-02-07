1323d14fe09bbda74fd0e81d1876b83ca37851d6
Givi Kuchukhidze <givik@users.noreply.github.com>
2026-02-07 07:42:15 +0400

    feat: Replace `AnimatedBook` with `Placeholder` components in the loading state and update associated styling.

--- app/[[...slug]]/loading.js
+++ app/[[...slug]]/loading.js
@@ -1,5 +1,6 @@
 import AnimatedBook from '@/components/AnimatedBook';
 import localFont from 'next/font/local';
+import Placeholder from '@/components/Placeholder';
 
 const textFont = localFont({
   src: '../fonts/bpg_nino_elite_round.otf',
@@ -8,8 +9,12 @@ const textFont = localFont({
 export default function Loading() {
   return (
     <div className="loading-content">
+      {/* <p className={textFont.className}>ბიბლია</p>
+      <AnimatedBook /> */}
+
+      <Placeholder />
       <p className={textFont.className}>ბიბლია</p>
-      <AnimatedBook />
+      <Placeholder />
     </div>
   );
 }
