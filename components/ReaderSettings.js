54aaee7c0b6d2b8788e986b905b8836938d5bd21
Givi Kuchukhidze <givik@users.noreply.github.com>
2026-02-08 09:41:58 +0400

    feat: Conditionally display ReaderSettings controls when settings are loaded and apply a minimum height to its container.

--- components/ReaderSettings.js
+++ components/ReaderSettings.js
@@ -14,33 +14,35 @@ const textFont = localFont({
 export default function ReaderSettings() {
     const { theme, toggleTheme, fontSize, updateFontSize, settingsLoaded } = useTheme();
 
-    if (!settingsLoaded) return null;
-
     return (
         <div
             className={`reader-settings ${textFont.className}`}
         // style={{ '--font-scale': fontSize }} // Moved to parent wrapper
         >
-            <div className="font-controls">
-                <button
-                    onClick={() => updateFontSize(-0.1)}
-                    aria-label="Decrease font size"
-                    className={textFont.className}
-                >
-                    ა-
-                </button>
-                <span>{(fontSize * 100).toFixed(0)}%</span>
-                <button
-                    onClick={() => updateFontSize(0.1)}
-                    aria-label="Increase font size"
-                    className={textFont.className}
-                >
-                    ა+
-                </button>
-            </div>
-            <button className={`theme-toggle ${bookFontBold.className}`} onClick={toggleTheme}>
-                {theme === 'light' ? <img width={30} src="/candle-2-svgrepo-com.svg" alt="candle" /> : <img width={30} src="/candle-2-off-svgrepo-com.svg" alt="candle-off" />}
-            </button>
+            {settingsLoaded && (
+                <>
+                    <div className="font-controls">
+                        <button
+                            onClick={() => updateFontSize(-0.1)}
+                            aria-label="Decrease font size"
+                            className={textFont.className}
+                        >
+                            ა-
+                        </button>
+                        <span>{(fontSize * 100).toFixed(0)}%</span>
+                        <button
+                            onClick={() => updateFontSize(0.1)}
+                            aria-label="Increase font size"
+                            className={textFont.className}
+                        >
+                            ა+
+                        </button>
+                    </div>
+                    <button className={`theme-toggle ${bookFontBold.className}`} onClick={toggleTheme}>
+                        {theme === 'light' ? <img width={30} src="/candle-2-svgrepo-com.svg" alt="candle" /> : <img width={30} src="/candle-2-off-svgrepo-com.svg" alt="candle-off" />}
+                    </button>
+                </>
+            )}
         </div>
     );
 }
